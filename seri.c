8e9798329983d4f254582fd27a07efcfe37bbbaf
Amanuel <amanuel.demirew@yahoo.com>
2023-08-24 22:42:25 +0300

    fix

--- seri.c
+++ seri.c
@@ -10,21 +10,8 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_gefi}, {"pall", f_teqr}, {"pint", f_kelmt},
-				{"pop", f_pop},
-				{"swap", f_swap},
-				{"add", f_add},
-				{"nop", f_nop},
-				{"sub", f_sub},
-				{"div", f_div},
-				{"mul", f_mul},
-				{"mod", f_mod},
-				{"pchar", f_pchar},
-				{"pstr", f_pstr},
-				{"rotl", f_rotl},
-				{"rotr", f_rotr},
+				{"push", f_gefi}, {"pall", f_teqr},,
 				{"queue", f_queue},
-				{"stack", f_stack},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
